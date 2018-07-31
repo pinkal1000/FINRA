0e690d305071034ba9500898a0d3e3907279c319
pinkal1000 <pinkal1000@yahoo.com>
2018-07-31 15:21:27 -0500

    Add files via upload

--- src/main/java/com/finra/interfaces/FinraReader.java
+++ src/main/java/com/finra/interfaces/FinraReader.java
@@ -1,7 +1,10 @@
 package com.finra.interfaces;
 import java.io.IOException;
 
+import com.fasterxml.jackson.core.JsonParseException;
+
 public interface FinraReader
 {
 	public String getShortJSON() throws IOException, InterruptedException;
+	public String getSymbols() throws JsonParseException, IOException;
 }
