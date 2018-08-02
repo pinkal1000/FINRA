3526945913453b72fae3240ef9fc20af12fe6a5f
pinkal1000 <pinkal1000@yahoo.com>
2018-08-02 14:07:14 -0500

    Externalized loader thread and improved load times

--- src/FinraReaderDAO.java
+++ src/FinraReaderDAO.java
@@ -47,15 +47,17 @@ public class FinraReaderDAO {
 		}
 		for (int i = 0; i < tm.getThreads().size(); i++)
 		{
-			while(!tm.getThreads().get(i).isDone)
+			while(!tm.getThreads().get(i).isDone())
 			{
 				Thread.sleep(500);
 				continue;
 			}
 			tm.removeThread(tm.getThreads().get(i));
 			System.out.println("threads remaining : " + tm.getThreads().size());
+			
 		}
 		long timeTaken = System.currentTimeMillis() - startTime;
+		dataMap = LoaderThread.getDataMap();
 		return "the time taken in ms to complete this load is " + timeTaken;
 	}
 	
@@ -65,11 +67,11 @@ public class FinraReaderDAO {
 		void addThread(LoaderThread lt)
 		{
 			threads.add(lt);
-			try {
-			Thread.sleep(100);
-			}
-			catch(InterruptedException ie)
-			{}
+//			try {
+//			Thread.sleep(10);
+//			}
+//			catch(InterruptedException ie)
+//			{}
 		}
 		ArrayList<LoaderThread> getThreads()
 		{
@@ -81,97 +83,7 @@ public class FinraReaderDAO {
 		}
 	}
 
-	class LoaderThread implements Runnable
-	{
-		String linkUrl;
-		String jsonData;
-		boolean isDone = false;
-		public void setLink(String url)
-		{
-			this.linkUrl = url;
-			
-		}
-		public void run()
-		{
-			try {
-				dataMap.put(linkUrl,loadFile(linkUrl));
-				System.out.println(linkUrl + " loaded string data of length " + dataMap.get(linkUrl).length());
-				isDone = true;
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		private String loadFile(String url) throws IOException
-		   {
-			   System.out.println("Processing file " + url);
-			   BufferedReader fs = null;
-			   URL shortDataURL = new URL( url);
-			   try 
-			   {
-				  fs  = new BufferedReader(
-				   new InputStreamReader(shortDataURL.openStream()));
-			   }
-			   catch (java.net.ConnectException ce)
-			   {
-				   try 
-				   {
-					   Thread.sleep(10);
-					   fs  = new BufferedReader(
-							   new InputStreamReader(shortDataURL.openStream()));
-				   }
-				   catch (InterruptedException ie)
-				   {
-					   
-				   }
-				   
-			   }
-				String s  = fs.readLine();
-				ArrayList<String> fields = new ArrayList<String>();
-				
-				StringTokenizer st  = new StringTokenizer(s,"|");
-				while (st.hasMoreTokens())
-				{
-					fields.add(st.nextToken());
-				}
-				String shortDataJSON = "[";
-				while ((s = fs.readLine()) != null)
-				{
-					jsonData += shortDataJSON;
-					shortDataJSON = "";
-					st  = new StringTokenizer(s,"|");
-					int j = 0;
-					while (st.hasMoreTokens())
-					{
 
-						if (j == 0)
-						    shortDataJSON += "{";
-						if (j < 3)
-							shortDataJSON += "\"" + fields.get(j) + "\": \"" + st.nextToken() + "\",";
-						if (j > 2 && j < 8) // numeric data
-						{	
-							String data = st.nextToken();
-							if (data.startsWith("."))
-								data = "0" + data;
-							if (data.startsWith("-."))
-								data = data.replace("-", "-0");
-							shortDataJSON += "\"" + fields.get(j) + "\": " + data + ",";
-						}
-						if (j == 8)
-							shortDataJSON += "\"" + fields.get(j) + "\": " + st.nextToken() + "},";
-						j++;
-					}
-					
-				}
-				int jsonDataSize = shortDataJSON.length();
-				shortDataJSON = shortDataJSON.substring(0, jsonDataSize - 1 );
-				shortDataJSON += "]";
-				jsonData += shortDataJSON;
-				fs.close();
-				return jsonData;
-
-		   }
-	}
 	
    
 	/**
@@ -196,12 +108,12 @@ public class FinraReaderDAO {
 			JsonToken jsonToken = parser.nextToken();
 			String fieldName = parser.getCurrentName();
 			String value = parser.getValueAsString();
-			if (fieldName != null && fieldName.equalsIgnoreCase("Security Symbol"))
+			if (fieldName != null && fieldName.equalsIgnoreCase("SecuritySymbol"))
 			{
 				if (value != null ) {
-					if (!value.equalsIgnoreCase("Security Symbol")) {
+					if (!value.equalsIgnoreCase("SecuritySymbol")) {
 						generator.writeStartObject();
-						generator.writeObjectField("Security Symbol", value);
+						generator.writeObjectField("SecuritySymbol", value);
 						generator.writeEndObject();
 					}
 				}
